5330f1cef3edaeedebaf94e5808f2e52f3429baf
MiaCoronel <coroneluchuypomamianicoll@gmail.com>
2026-06-29 19:29:49 -0500

    feat: estructura base del proyecto Angular

---
+++ src/app/components/camera-card/camera-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-card',
+  imports: [],
+  templateUrl: './camera-card.html',
+  styleUrl: './camera-card.scss',
+})
+export class CameraCard {
+
+}
---
+++ src/app/pages/cameras/cameras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cameras',
+  imports: [],
+  templateUrl: './cameras.html',
+  styleUrl: './cameras.scss',
+})
+export class Cameras {
+
+}
