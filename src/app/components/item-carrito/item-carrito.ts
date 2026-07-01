d53fcaf6bcd7d9589503bb7610e067b7eed5b208
MiaCoronel <coroneluchuypomamianicoll@gmail.com>
2026-07-01 13:42:14 -0500

    feat: agregar componentes base

---
+++ src/app/components/item-carrito/item-carrito.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-carrito',
+  imports: [],
+  templateUrl: './item-carrito.html',
+  styleUrl: './item-carrito.css',
+})
+export class ItemCarrito {
+
+}
---
+++ src/app/components/lista-resenas/lista-resenas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-resenas',
+  imports: [],
+  templateUrl: './lista-resenas.html',
+  styleUrl: './lista-resenas.css',
+})
+export class ListaResenas {
+
+}
